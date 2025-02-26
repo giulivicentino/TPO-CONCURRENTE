074c87593b8a2a0beee1d1f7f154c187a1a07cb9
giulivicentino <giulivicentinoo@gmail.com>
2025-02-26 17:00:53 -0300

    ????

--- ECOPARQUE/Persona.java
+++ ECOPARQUE/Persona.java
@@ -1,8 +1,4 @@
 import java.util.Random;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Persona extends Thread {
     
@@ -10,49 +6,134 @@ public class Persona extends Thread {
      private Colectivo cole;
 
 
-    public Persona(String n,Colectivo cole,Parque unParque) {
+    public Persona(String n, NadoDelfines p, int h, Restaurante res, Laguna l, MundoAventura m, int ladoTirolesa,
+            CarreraGomones c, int eleccionTransporte, int eleccionGomon, FaroMirador faro) {
+        this.pile = p;
+        this.horarioPile = h;
         this.setName(n);
         this.cole=cole;
         this.parque = unParque;
             }
 
     public void run() {
-        Random r = new Random();
-        boolean porCole = r.nextBoolean();
 
-        if(porCole){
-            System.out.println("La persona: "+Thread.currentThread().getName().toString()+" se dirige a la fila del colectivo");
-            try {
-                System.out.println("La persona: "+Thread.currentThread().getName().toString()+" SUBIO al colectivo");
-                boolean cole1=cole.subirCole();
-                Thread.sleep(3000);
-                cole.bajarCole(cole1);
-                System.out.println("La persona: "+Thread.currentThread().getName().toString()+" llega a destino con el tour");
-            } catch (InterruptedException e) { e.printStackTrace();} 
-        }else{
-            System.out.println("La persona: "+Thread.currentThread().getName().toString()+" accede de manera particular");
-        }
-        
-        // se cambia el eleccion aleatorio hasta que un numero salga (6) que determine que ya se quiere ir
+        // -------------------NADO DELFINES
+        // pile.solPile(); // desp habria q agregarle q sea random lo q deciden hacer
 
-        Random r1 = new Random();//2
-        int eleccionResto= r1.nextInt(0,3);
-        
-        Random r3 = new Random();//3
-        int ladoTirolesa = r3.nextInt(1,2);
-        
-        Random r4 = new Random();// 4
-        int eleccionTransporte= r4.nextInt(1,2);
+        // -------------------SNORKEL
 
-        Random r5 = new Random();//4
-        int eleccionGomon= r5.nextInt(1,2);
+        /*
+         * try {
+         * 
+         * laguna.solicitarEquipo();
+         * Thread.sleep(2000);
+         * laguna.devolverEquipo();
+         * 
+         * } catch (InterruptedException e) {
+         * e.printStackTrace();
+         * }
+         * 
+         * }
+         * 
+         * 
+         * 
+         * //-------------------MUNDO AVENTURA
+         * 
+         * try {
+         * 
+         * 
+         * switch (ladoTirolesa) {
+         * case 1:
+         * System.out.println("p este");
+         * ma.subirTirolesa();
+         * Thread.sleep(200);
+         * ma.bajarseTirolesa();
+         * break;
+         * 
+         * case 2:
+         * System.out.println("p oeste");
+         * ma.subirTirolesa2();
+         * Thread.sleep(200);
+         * ma.bajarseTirolesa2();
+         * 
+         * 
+         * break;
+         * }
+         * 
+         * ma.usarCuerda();
+         * Thread.sleep(400);
+         * ma.dejarCuerda();
+         * 
+         * ma.saltar();
+         * Thread.sleep(200);
+         * ma.dejarSalto();
+         * 
+         * 
+         * } catch (Exception e) {
+         * 
+         * }
+         * 
+         * //-------------------RESTAURANTE
+         * try {
+         * 
+         * resto.entrarRestaurante(this);
+         * resto.pedirAlmuerzo(this);
+         * resto.salirRestaurante();
+         * 
+         * Thread.sleep(2000);// lo dejo asi para probar, pero tendria que ser que lo
+         * puedan pedir entre
+         * // actividades tmb
+         * 
+         * resto.entrarRestaurante(this);
+         * resto.pedirMerienda(this);
+         * resto.salirRestaurante();
+         * 
+         * } catch (Exception e) {
+         * 
+         * }
+         */
+
+        // -------------------CARRERA DE GOMONES
 
         try {
-            parque.realizarActividad(1, this,eleccionResto,ladoTirolesa,eleccionTransporte,eleccionGomon);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            switch (eleccionTransporte) {
+                case 1:
+                    cg.subirTren();
+                    break;
+
+                case 2:
+                    cg.subirBici();
+                    Thread.sleep(6000);
+                    cg.dejarBici();
+                    break;
+            }
+        } catch (Exception e) {
         }
+
+        
+         try {
+            switch (eleccionGomon) {
+                case 1: // caso gomon doble
+                    if (cg.elegirGomon(true)) {
+                        cg.carrera();
+                        cg.devolverGomon(true);
+
+                    } else {
+                        Thread.sleep(4000);
+                    }
+                    break;
+
+                case 2: // caso gomon simple
+                    cg.elegirGomon(false);
+                    cg.carrera();
+                    cg.devolverGomon(false);
+                    break;
+            }
+
+        } catch (Exception e) {
+        }
+
+         
 
     }
 }
