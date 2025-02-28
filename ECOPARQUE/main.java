d420ddcc5738c7c282a3a1c8a9a5384a6547d830
tomasocchi <tomasocchi15@gmail.com>
2025-02-28 17:15:08 -0300

    main chelo

--- ECOPARQUE/main.java
+++ ECOPARQUE/main.java
@@ -16,57 +16,55 @@ import Recursos.Restaurante;
 import Recursos.Tiempo;
 
 public class main {
-    /**
-     * @param args
-     */
 
     public static void main(String[] args) {
-        
-        //recursos compartidos 
+
+        // recursos compartidos
         Tiempo t = new Tiempo(9, 0);
         NadoDelfines nd = new NadoDelfines(t);
         Restaurante[] colRestaurantes = new Restaurante[3];
-        colRestaurantes[0] = new Restaurante(1, 2,t);
-        colRestaurantes[1] = new Restaurante(2, 5,t);
-        colRestaurantes[2] = new Restaurante(3, 7,t);
-        Laguna laguna = new Laguna(t); 
-        MundoAventura ma = new MundoAventura(t); 
-        CarreraGomones cg = new CarreraGomones(t); 
+        colRestaurantes[0] = new Restaurante(1, 2, t);
+        colRestaurantes[1] = new Restaurante(2, 5, t);
+        colRestaurantes[2] = new Restaurante(3, 7, t);
+        Laguna laguna = new Laguna(t);
+        MundoAventura ma = new MundoAventura(t);
+        CarreraGomones cg = new CarreraGomones(t);
         FaroMirador fa = new FaroMirador(t);
         Colectivo cole = new Colectivo();
         Parque ecoParque = new Parque(nd, colRestaurantes, laguna, ma, cg, fa);
-        
-        
-        // hilos personas
+
+        //hilos personas
         Persona[] p = new Persona[400];
 
         for (int i = 0; i < p.length; i++) {
-                p[i] = new Persona("persona " + (i),cole, ecoParque,t);
+            p[i] = new Persona("persona " + (i), cole, ecoParque, t);
         }
-        
-        
-        
-        //hilos controles 
-        ControlTiempo tiempo = new ControlTiempo(t, nd); 
+
+        // hilos controles
+        ControlTiempo tiempo = new ControlTiempo(t, nd);
         ControlPileta cp = new ControlPileta(nd);
-        AsistenteSnorkel[] asistentesSnorkel = new AsistenteSnorkel[2]; 
-        ControlTirolesa ct = new ControlTirolesa(ma); 
-        ControlTren ctren = new ControlTren(cg); 
+        AsistenteSnorkel[] asistentesSnorkel = new AsistenteSnorkel[2];
+        ControlTirolesa ct = new ControlTirolesa(ma);
+        ControlTren ctren = new ControlTren(cg);
         ControlFaro cFaro = new ControlFaro(fa);
-        
-        for(int i= 0; i < asistentesSnorkel.length; i++){
-            asistentesSnorkel[i] = new AsistenteSnorkel(laguna); 
-        } 
+
+        for (int i = 0; i < asistentesSnorkel.length; i++) {
+            asistentesSnorkel[i] = new AsistenteSnorkel(laguna);
+        }
 
         // comienzo de hilos controles
+       
         cp.start(); // control pileta
+
         for (int i = 0; i < asistentesSnorkel.length; i++) {
-            asistentesSnorkel[i].start();
+            asistentesSnorkel[i].start(); //asistentes snorkel
         }
+
         ct.start(); // control tirolesa
         ctren.start(); // control tren
         cFaro.start(); // control faro
-        tiempo.start();
+        tiempo.start();  //control tiempo
+
         // comienzo de hilos personas
         for (int i = 0; i < p.length; i++) {
             p[i].start();
